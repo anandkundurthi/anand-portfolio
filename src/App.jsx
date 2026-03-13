55efa7a0d2f6346049424bea384956837e430386
Anand Sarma <anandsarma@Anands-MacBook-Air.local>
2026-03-13 20:54:33 +0530

    Fix EmailJS template id

--- src/App.jsx
+++ src/App.jsx
@@ -613,7 +613,7 @@ function ContactForm() {
         });
         window.emailjs.init("Mffsv4BJ2iFYVa-xe");
       }
-      await window.emailjs.send("service_fgf1mpm", "template_gp18zim", {
+      await window.emailjs.send("service_fgf1mpm", "template_iaq92rz", {
         from_name: form.name,
         from_email: form.email,
         message: form.message,
